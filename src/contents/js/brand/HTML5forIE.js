a70471d891934bb80c26cc72f88bd57a6cc34d08
youngjinsong <ssongki@gmail.com>
2018-08-29 20:56:56 +0900

    *Update - IE Version Check

--- src/contents/js/brand/HTML5forIE.js
+++
@@ -1,8 +0,0 @@
-( function() {
-	if(!/*@cc_on!@*/0) return;
-	var e = "article,aside,audio,canvas,datagrid,datalist,details,dialog,"+"eventsource,figure,figcaption,footer,header,hgroup,mark,"+"meter,nav,output,progress,section,time,video";
-	var elem = e.split(',');
-	for(var i=0; i<elem.length; i++){
-		document.createElement(elem[i]);
-	}
-})()
